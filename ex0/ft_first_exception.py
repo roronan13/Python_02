f393aa738d3f1889ef0471015c506823882ac730
roronan13 <ronan.pothier@gmail.com>
2026-03-27 22:33:12 +0100

    dacc

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,3 +1,13 @@
+#!usr/bin/env python3
 
+def input_temperature(temp_str: str) -> int:
+    return int(temp_str)
 
-def in
+def test_temperature() -> None:
+    
+    
+
+if __name__ == "__main__":
+    print(" === Garden temperature === \n")
+    test_temperature("25")
+    test_temperature("abc")
