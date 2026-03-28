15ffe0a58245ede1333669a05ea3131da6f97030
roronan13 <ronan.pothier@gmail.com>
2026-03-28 12:03:23 +0100

    ok

--- ex0/ft_first_exception.py
+++ ex0/ft_first_exception.py
@@ -1,11 +1,16 @@
 #!usr/bin/env python3
 
 def input_temperature(temp_str: str) -> int:
+    print(f"Input date is {temp_str}")
     return int(temp_str)
 
-def test_temperature() -> None:
-    
-    
+def test_temperature(temperature: str) -> None:
+    try:
+        temp_int = input_temperature(temperature)
+        print(f"Temperature is now {temp_int}°C")
+    except ValueError:
+        print("NO")
+
 
 if __name__ == "__main__":
     print(" === Garden temperature === \n")
